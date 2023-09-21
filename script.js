7a70c899fdf3897c702e05f57afe7a5fa1b6b932
Andikanugh <75620540+Timikaro@users.noreply.github.com>
2023-09-21 11:23:04 +0700

    First Commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const links = document.querySelectorAll(".company a");
+
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop - 80;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth"
+  });
+}
+
+// function myFunction() {
+//   alert("Page is loaded");
+  
+// }
+$(document).ready(function(){
+  alert("halaman siap di tampilkan");
+})
